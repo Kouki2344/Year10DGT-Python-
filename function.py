855d2060695254673648504392405cbeb4b48a9e
Kouki2344 <168596541+Kouki2344@users.noreply.github.com>
2024-05-08 10:05:06 +1200

    Add files via upload

---
+++ function.py
@@ -0,0 +1,15 @@
+sum = "5"+"5"
+print(sum)
+
+sum = 5 + 5
+
+#functions go here
+def intro():
+    print("Welcome")
+    print()
+    print("Insturctions go here")
+    return "" #I am returning (displaying/using) all items 
+                #in the code
+
+# Main routine
+intro() # This calls the function so that we can use it.
