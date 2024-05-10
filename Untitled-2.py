2a2675b82329f8353f92f5b621da13c7e64879cc
Kouki2344 <168596541+Kouki2344@users.noreply.github.com>
2024-05-10 14:21:22 +1200

    Add files via upload

---
+++ Untitled-2.py
@@ -0,0 +1,10 @@
+greeting = "hello world"
+fav_num = 7
+love_programming = True
+print(greeting)
+print(fav_num)
+print(love_programming)
+num_string = "5"
+print(fav_num * 2)
+print(num_string * 5)
+PI=
