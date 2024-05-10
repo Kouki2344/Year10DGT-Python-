0b189192290f3ba6472b35da41a0baa40ff3f8c1
Kouki2344 <168596541+Kouki2344@users.noreply.github.com>
2024-05-10 14:40:16 +1200

    Add files via upload

---
+++ Input output.py
@@ -0,0 +1,8 @@
+username = input("what is your name?")
+fav_num = int(input("How are you today?"))
+double = fav_num * 2
+halve = fav_num / 2
+square = fav_num * fav_num
+
+# Greet the user
+print("Hi {username}, your favourite number is {fav_num}")
