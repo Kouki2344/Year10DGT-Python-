71d91ce40c56ac7599543692adfb3b146ed35581
Kouki2344 <168596541+Kouki2344@users.noreply.github.com>
2024-05-17 15:06:08 +1200

    Add files via upload

---
+++ statement generator.py
@@ -0,0 +1,7 @@
+# function go here
+
+def statement_generator(statement, decoration):
+    print(f"\n{decoration * 5} {statement} {decoration * 5}")
+
+# Main rountine goes here
+statement_generator ("Instructions", "*")
